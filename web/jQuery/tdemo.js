92a5b2041042fd452b6e2d05075a56ff992e4830
Thasnim <thasnimthasim625@gmail.com>
2024-06-03 14:01:57 +0530

    first commit

---
+++ web/jQuery/tdemo.js
@@ -0,0 +1,169 @@
+// $("#para").click(function()
+// {
+
+//   $("p").hide()
+// })
+
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     // $("p:first").hide();
+//     // $("[href]").hide();
+//     // $("button").hide();
+//   })
+// })
+
+
+// $(function(){
+//   $("button").dblclick(function(){ //double click event
+//     // $("p:first").hide();
+//     // $("[href]").hide();
+//     $("button").hide();
+//   })
+// })
+
+// $("p").hover(function(){
+//   alert("You entered p1!");
+// },
+// function(){
+//   alert("Bye! You now leave p1!");
+// });
+
+
+// $(function(){
+// $("p").on({
+//   mouseenter: function(){
+//     $(this).css("color","yellow")
+//   },
+//   mouseleave:function(){
+//     $(this).css("background-color" ,"black")
+//   },
+//   click:function(){
+//     $(this).css("background-color","red")// not working
+//   }
+// })
+// })
+
+
+// $(function(){
+// $("p").on({
+//   mouseenter: function(){
+//     $(this).css("background-color", "lightgray");
+//   },
+//   mouseleave: function(){
+//     $(this).css("background-color", "lightblue");
+//   },
+//   click: function(){
+//     $(this).css("background-color", "yellow");
+//   }
+// })
+// // })
+
+
+// $(function() {
+//   $("input").focus(function(){
+//     $(this).css("background-color", "lightblue");
+//   }
+
+//   )
+// })
+
+
+
+// $(function() {
+//   $("input").blur(function(){
+//     $(this).css("background-color", "lightblue");
+//   }
+
+//   )
+// })
+ 
+
+
+
+
+// $(function() {
+//   $("input").keypress(function(){
+//     $(this).css("background-color", "lightblue");
+//   }
+
+//   )
+// })
+
+
+
+// $(function() {
+//   $("input").keyup(function(){
+//     $(this).css("background-color", "lightblue");
+//   }
+
+//   )
+// })
+
+// $("#hide").click(function(){
+//   $("p").hide(7000)
+// });
+// $("#show").click(function(){
+//   $("p").show()
+// })
+
+// $(function(){
+//   $("button").click(function(){
+//     $("p").toggle()
+//   })
+// })
+
+
+// $(function(){
+//   $(".div").click(function(){
+//     $("#div1").fadeIn(5000);
+//     $("#div2").fadeIn("slow");
+//     $("#div3").fadeIn();
+//   })
+// })
+
+
+// $(function(){
+//   $(".div").click(function(){
+//     $("#div1").fadeOut();
+//     $("#div2").fadeOut("fast")
+//     $("#div3").fadeOut(3000)
+
+//   })
+// })
+
+
+ 
+// $(function(){
+//   $(".div").click(function(){
+//     $("#div1").fadeToggle();
+//     $("#div2").fadeToggle("slow")
+//     $("#div3").fadeToggle(3000)
+
+//   })
+// })
+
+ 
+
+
+// $(function(){
+//   $(".div").click(function(){
+//     $("#div1").fadeTo("slow",0.5)
+//   })
+
+// })
+
+
+// $(function(){
+//   $("#flip").click(function(){
+//     $("#panel").slideDown()
+//   })
+// })
+
+
+
+$(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle()
+  })
+})
+  
